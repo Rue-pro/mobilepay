e9b0b4b59fcf62f0ea84767555fcbb9179838dfc
unknown <ruesullivanpro@gmail.com>
2021-10-20 11:37:00 +0400

    fix: remove console.log from operators api

--- pages/api/operators.ts
+++ pages/api/operators.ts
@@ -15,7 +15,6 @@ export const api = {
     return operators;
   },
   getOperator(id: string) {
-    console.log(operators);
     return operators.find((op) => op.id === Number(id)) || null;
   },
   createOperator(operator: OperatorNew) {
